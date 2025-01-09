6a8f984bce5933f4e3c88f0500f4cf80e3e9fa65
Sione-Uhlig <uhl24001@byui.edu>
2025-01-09 12:46:03 -0600

    Exercise 5 complete.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+        string userName = GetUserName();
+        int UserNumber =  FavoriteNumber();
+        int SquaredNumber = NumberSquared(UserNumber);
+        Display(userName, SquaredNumber);
     }
+
+    static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int FavoriteNumber()
+    {
+        Console.Write("please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int NumberSquared(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void Display(string name, int square)
+    {
+        Console.Write($"{name}, the square of your number is {square}");
+    }
+
+
 }
