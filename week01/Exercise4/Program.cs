5f22dfb26f0d4a3c63d2bed75290d4d79b91baff
Sione-Uhlig <uhl24001@byui.edu>
2025-01-09 11:10:28 -0600

    Exercise 3 complete.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int UserInput;
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            UserInput = int.Parse(Console.ReadLine());
+
+            if (UserInput == 0)
+            {
+                break;
+            }
+
+            numbers.Add(UserInput);
+        }
+
+        double avg = numbers.Average();
+        int sum = numbers.Sum();
+        int largest = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}.");
+        Console.WriteLine($"The average is: {avg}.");
+        Console.WriteLine($"The largest number is: {largest}.");
+
     }
 }
