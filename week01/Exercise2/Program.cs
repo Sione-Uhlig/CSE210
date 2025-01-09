d2e31f540cb98cc1ba8b2cb452ae78f6929123da
Sione-Uhlig <uhl24001@byui.edu>
2025-01-09 09:11:57 -0600

    Exercise two complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("\nWhat was the percentage you receive?. ");
+        string input = Console.ReadLine();
+
+        int grade = int.Parse(input);
+
+        string LetterGrade = "";
+
+        if (grade >= 90)
+        {
+            LetterGrade = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            LetterGrade = "B";
+        }
+
+          else if (grade >= 70)
+        {
+            LetterGrade = "C";
+        }
+
+          else if (grade >= 60)
+        {
+            LetterGrade = "D";
+        }
+
+          else
+        {
+            LetterGrade = "F";
+        }
+
+        Console.WriteLine($"\nYou received a(n) {LetterGrade} for your class. ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the class! \n");
+        }
+
+        else 
+        {
+            Console.WriteLine("you will need to try again next semester. \n");
+        }
+
     }
 }
