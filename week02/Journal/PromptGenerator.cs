d25534d35b4a751dbf52985d9adc68ff6ff7696c
Sione-Uhlig <uhl24001@byui.edu>
2025-01-16 17:27:40 -0600

    Week 2 update 1

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,25 @@
+using System;
+
+class PromptGenerators
+{
+   List<string> Prompts = new List<string>();
+
+   public PromptGenerators()
+   {
+        Prompts.Add("Who was the most interesting person I interacted with today?");
+        Prompts.Add("What was the best part of my day?");
+        Prompts.Add("How did I see the hand of the Lord in my life today?");
+        Prompts.Add("What was the strongest emotion I felt today?");
+        Prompts.Add("If I had one thing I could do over today, what would it be?");
+   }
+
+   public void DisplayRandomPrompt()
+   {
+    foreach ( string Prompt in Prompts)
+    {
+        Console.WriteLine(Prompt);
+    }
+   }
+
+
+}
