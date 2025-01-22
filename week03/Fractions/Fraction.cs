c370b16bd8803da129a4630371535c467e01c536
Sione <sonywells@gmail.com>
2025-01-22 14:27:57 -0600

    Fractions update 1

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,13 @@
+using System;
+public class Fraction
+{
+    private int _topNumber;
+    private int _bottomNumber;
+
+    public Fraction()
+    {
+        _topNumber = 1;
+        _bottomNumber = 1;
+    }
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1);
     }
 }
