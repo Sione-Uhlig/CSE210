6b16c997f1bac335e04fa3b808963af45f6a01f5
Sione <sonywells@gmail.com>
2025-01-23 12:56:37 -0600

    Fractions final update

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -10,4 +10,27 @@ public class Fraction
         _bottomNumber = 1;
     }
 
+    public Fraction(int wholeNumber)
+    {
+        _topNumber = wholeNumber;
+        _bottomNumber = 1;
+    }
+
+    public Fraction(int topNumber, int bottomNumber)
+    {
+        _topNumber = topNumber;
+        _bottomNumber = bottomNumber;
+    }
+
+    public string GetFraction()
+    {
+        string fraction = $"{_topNumber}/{_bottomNumber} ";
+        return fraction;
+    }
+
+    public double GetDecimal()
+    {
+        return (double)_topNumber / (double)_bottomNumber;
+    }
+
 }
