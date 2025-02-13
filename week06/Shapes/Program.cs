fc189eb10d36bf0b62c483a54def3d6865b352b9
Sione-Uhlig <uhl24001@byui.edu>
2025-02-13 17:39:32 -0600

    shapes final update

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 2);
+        shapes.Add(square);        
+
+        Rectangle rectangle = new Rectangle("Blue", 2, 4);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} Shape has an area of {area}.");
+        }
+
+        
+        
     }
 }
